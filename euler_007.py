ba614a191916acc77f0bede851f05fbf4d1cc751
Jit Kanetkar <kanetkarster@gmail.com>
2013-07-01 20:27:28 -0400

    math.sqrt is significantly faster than ** .5, so I replaced the function

--- euler_007.py
+++ euler_007.py
@@ -1,10 +1,13 @@
-num = int(raw_input("find prime number: "))
+import time
+from math import sqrt
+time.clock()
+num = 10001
 primes = [2, 3]
 k = 5
 while(len(primes) < (num)):
 	is_prime = True
 	for prime in primes:
-		if(prime > (k ** .5)):
+		if(prime > (sqrt(k))):
 			break
 		if(k%prime == 0):
 			is_prime = False
@@ -13,4 +16,5 @@ while(len(primes) < (num)):
 		primes.append(k)
 	k += 2
 #print primes
+print time.clock()
 print primes[num - 1]
